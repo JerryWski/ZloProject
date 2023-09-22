b992b499835cb0144626594aa9c221c65feed0e3
JerryWski <jarek_wasilewski@yahoo.com>
2023-09-22 22:24:01 +0200

    some polishing

--- src/components/SwiperModule.tsx
+++ src/components/SwiperModule.tsx
@@ -21,42 +21,18 @@ const SwiperModule = () => {
       modules={[Pagination, Autoplay]}
       className='mySwiper'
     >
-        {clients.map((client) => (
-      <SwiperSlide>
+      {clients.map((client) => (
+        <SwiperSlide>
           <div className='swiper-container' key={client.id}>
             <p className='swiper-text'>{client.text}</p>
             <div className='img-wrapper'>
-              <img src={client.image} alt="" />
+              <img src={client.image} alt='' />
             </div>
             <h2 className='swiper-header'>{client.header}</h2>
             <h3 className='swiper-subheader'>{client.subheader}</h3>
           </div>
-      </SwiperSlide>
-        ))}
-      {/* <SwiperSlide>
-        <p>
-          "Pełny profesjonalizm, duże zaangażowanie, znajomość marketingu i
-          ogromne doświadczenie - to tylko niektóre cechy, które posiada Maciej
-          Martowicz. Z całego serca polecam współpracę z tym człowiekiem - on
-          wie, co robi i każda chwila spędzona z nim wzbogaci Cię o rok studiów
-          !"
-        </p>
-        <div>
-          <img src='' alt='' />
-        </div>
-        <h2>Andrzej Makarenko</h2>
-        <h3>CEO & Founder Szkoła Językowa Speech Language Studio</h3>
-      </SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
-      <SwiperSlide>Slide 10</SwiperSlide>
-      <SwiperSlide>Slide 11</SwiperSlide>
-      <SwiperSlide>Slide 12</SwiperSlide> */}
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
