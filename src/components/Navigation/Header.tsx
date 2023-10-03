380e835c1e1ee460f0558bf2cc89bf6012a3c43d
JerryWski <jarek_wasilewski@yahoo.com>
2023-10-03 19:27:43 +0200

    polishing

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -32,7 +32,7 @@ const Header: React.FC = () => {
             <p className='heading'>
               <span className='span_hash'>#</span>CoachSamoZuo
             </p>
-            <p className='subheading'>Make's a difference</p>
+            <p className='subheading'>make's a difference</p>
           </a>
         </div>
         <div className='contact-wrapper'>
