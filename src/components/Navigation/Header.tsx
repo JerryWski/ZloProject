b1eebb5b956f0b1f98a40e71be7bf0a1813ca77d
JerryWski <jarek_wasilewski@yahoo.com>
2023-10-06 20:35:37 +0200

    styling

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -29,14 +29,14 @@ const Header: React.FC = () => {
       <nav className='nav-container'>
         <div className='logo-container'>
           <a href='/'>
-            <div className='aloha-logo-container'>
+            <div className='aloha-logo-wrapper'>
               <img src='/aloha-logo.png' alt='logo-icon' />
             </div>
-            <p className='heading'>
+            <p className='logo-heading'>
               <span className='span_hash'>#</span>CoachSamoZuo
             </p>
 
-            <p className='subheading'>makes a difference</p>
+            <p className='logo-subheading'>makes a difference</p>
           </a>
         </div>
         <div className='contact-wrapper'>
