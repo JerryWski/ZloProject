0e810db50839ec2eb3551856e15188fbf030f5d2
JerryWski <jarek_wasilewski@yahoo.com>
2023-10-13 21:46:49 +0200

    going to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
+  // output: 'server',
+  // adapter: netlify(),
   integrations: [react()],
 });
